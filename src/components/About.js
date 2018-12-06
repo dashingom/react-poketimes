064b21404088ead0b1a08153159c645dae6efb1c
dashingom <onkar.nawghare@gmail.com>
2018-12-06 21:42:27 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow' 
+
+const About = () => {
+  return(
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde odio aliquam debitis dolores omnis iure labore, quam harum! Ut, eius nesciunt corporis minima harum obcaecati reiciendis architecto numquam modi!</p>
+    </div>
+  )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return(
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde odio aliquam debitis dolores omnis iure labore, quam harum! Ut, eius nesciunt corporis minima harum obcaecati reiciendis architecto numquam modi!</p>
+    </div>
+  )
+}
+
+export default Contact;
